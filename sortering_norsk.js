3b2a10e22871af2bd09979361bd50f0096248df6
Terje Berg-Hansen <terje@itfakultetet.no>
2018-01-29 18:09:17 +0100

    Norsk sortering av array

---
+++ sortering_norsk.js
@@ -0,0 +1,23 @@
+let folk = ["Åge","Mikkel","Kari","Jens","Øivind","Ola"];
+
+
+console.log("Usortert\n");
+
+console.log(folk);
+
+console.log("\nDefault sortering\n");
+
+console.log(folk.sort());
+
+console.log("\nNorsk sortering\n");
+
+console.log(folk.sort((a, b) => a.localeCompare(b, "nb")));
+
+
+// med norsk som en egen funksjon
+let norskSort = (a, b) => a.localeCompare(b, "nb");
+
+console.log("\nNorsk sortering - egen funksjon\n");
+
+console.log(folk.sort(norskSort));
+
