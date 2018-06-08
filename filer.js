121ac2c7e0a9c8bc9fabeaf7852925ad579418c2
Terje Berg-Hansen <terje@itfakultetet.no>
2018-06-08 16:20:26 +0200

    Add files via upload

--- filer.js
+++ filer.js
@@ -1,12 +1,20 @@
 const fetch = require('node-fetch');
 
-fetch('https://itfakultetet.no/index.php')
-.then(data => {
-        // Do some work with the returned data
-        console.log(data.body);
-        
-    })
+fetch('https://itfakultetet.no/dogs.json')
+.then(data => data.json())
+.then(json => {
+console.log("Struktur:");
+
+  console.log(json.dogs);
+  
+console.log("\nBla gjennom med forEach:");
+
+json.dogs.forEach(hund => {
+  console.log("Navn:",hund.navn,"- Alder:",hund.alder);
+  
+});
+
+}) 
 .catch(error => {
         // Process all errors here
     });
-
