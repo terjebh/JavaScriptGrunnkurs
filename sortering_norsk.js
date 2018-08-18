617635c88193f180465147daf9811960d9784f0e
terje@itfakultetet.no <terje@itfakultetet.no>
2018-08-18 03:32:16 +0200

    ny

--- sortering_norsk.js
+++ sortering_norsk.js
@@ -1,4 +1,6 @@
-let folk = ["Åge","Mikkel","Kari","Jens","Øivind","Ola"];
+// Eskempler på sortering med ulike språk
+
+let folk = ["Åge","Aage","Mikkel","Kari","Jens","Øivind","Ola"];
 
 console.log("Usortert\n");
 
@@ -10,13 +12,22 @@ console.log(folk.sort());
 
 console.log("\nNorsk sortering\n");
 
-console.log(folk.sort((a, b) => a.localeCompare(b, "nb")));
+console.log(folk.sort((a, b) => a.localeCompare(b, "no")));
 
 
 // med norsk som en egen funksjon
-let norskSort = (a, b) => a.localeCompare(b, "nb");
+let norskSort = (a, b) => a.localeCompare(b, "no");
 
 console.log("\nNorsk sortering - egen funksjon\n");
 
 console.log(folk.sort(norskSort));
+
+// med svensk som en egen funksjon
+let svenskSort = (a, b) => a.localeCompare(b, "sv");
+
+console.log("\nSvensk sortering - egen funksjon\n");
+
+console.log(folk.sort(svenskSort));
+
+
 
