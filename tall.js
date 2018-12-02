bb342a8792b44a1424c996c1be192c06657acd9a
Terje Berg-Hansena <terje@itfakultetet.no>
2018-12-02 19:31:12 +0100

    Tallfunksjoner

--- tall.js
+++ tall.js
@@ -15,5 +15,16 @@ console.log(Math.abs(num)); // 7.987654
 console.log(Math.ceil(num)); // 8 
 console.log(Math.floor(num)); // 7 
 console.log(Math.sign(num)); // 1 = positivt tall 
-console.log(Math.trunc(num)); // 8 
+console.log(Math.trunc(num)); // 7 
+
+// Tilfeldige nummer
+console.log(Math.random()); // tilfeldig desimal-tall mellom 0 og 1  
+
+// min og max
+console.log(Math.max(3,5,76,5,12,4)); // 76 
+console.log(Math.min(3,5,76,5,12,4)); // 3 
+
+// Snitt (med reduce-funksjon)
+let avg = (array) => array.reduce((a, b) => a + b) / array.length;
+console.log(avg([1,2,3,4,5])); // 3
 
