8e38e5cb68bbfa9bdfed39ebdd994effc907767e
Terje Berg-Hansena <terje@itfakultetet.no>
2018-12-02 16:40:49 +0100

    Tallfunksjoner

--- tall.js
+++ tall.js
@@ -2,6 +2,10 @@
 
 // Presisjonsnivå
 let num = new Number(7.123456);
-console.log(num.toPrecision(4));
+console.log(num.toPrecision(4)); // 7.123
+
+// Avrunding
+console.log(num.toFixed()); // 7    
+console.log(Math.round(num,2)); // 7 
 
 
