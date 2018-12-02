884b99d94f815e6e1d3816866e10b8115dfe3676
Terje Berg-Hansena <terje@itfakultetet.no>
2018-12-02 17:06:18 +0100

    Tallfunksjoner

--- tall.js
+++ tall.js
@@ -11,4 +11,9 @@ console.log(num.toPrecision(1)); // 8
 
 console.log(Math.round(num)); // 8 
 
+console.log(Math.abs(num)); // 7.987654
+console.log(Math.ceil(num)); // 8 
+console.log(Math.floor(num)); // 7 
+console.log(Math.sign(num)); // 1 = positivt tall 
+console.log(Math.trunc(num)); // 8 
 
