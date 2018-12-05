a50d0a962b362c5d6b3a7381e3f6112948f1725f
Terje Berg-Hansena <terje@itfakultetet.no>
2018-12-05 22:23:33 +0100

    Ulike eksempler på funksjoner i objekter

--- tester.js
+++ tester.js
@@ -1,6 +1,9 @@
 
 let dyr = [
-{navn:"Petter",art:"Katt",alder:2},
+{navn:"Petter",art:"Katt",alder:2,lyd:"Miau,Miau", snakk : function () { 
+    console.log(this.lyd); 
+}  
+},
 {navn:"Ola",art:"Hund",alder:4},
 {navn:"Truls",art:"Katt",alder:3},
 {navn:"Dagros",art:"Ku",alder:11}
@@ -9,5 +12,22 @@ let dyr = [
 console.log(dyr.map(x => x.navn+" er en "+x.art+" som er "+x.alder+" år gammel").join("\n"));
 
 console.log("\nSamlet alder for alle dyr: ",dyr.map(x=>x.alder).reduce((x,y) => x+y));
+
+// this og bind
+
+let Petter = dyr.filter(x => x.navn == "Petter")[0];
+Petter; 
+
+Petter.snakk();  // Miau, Miau
+
+let siMiau = Petter.snakk; 
+
+siMiau(); // undefined
+
+let siMiauBundet = siMiau.bind(Petter); 
+
+siMiauBundet(); // Miau, Miau
+ 
+
 
 
