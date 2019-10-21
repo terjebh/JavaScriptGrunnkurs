8c8811eae1bef32383103d31ede19e8f82122506
Terje Berg-Hansen <terje@itfakultetet.no>
2019-10-21 18:16:29 +0200

    Arrays ned funksjonell porgrammering

--- funksjonell_javascript.js
+++ funksjonell_javascript.js
@@ -8,34 +8,38 @@ let dyr = [
 ];
 
 
-// Hent deler av objektene med map() og skriv ut som et array
-console.log("Skrevet ut som array med map():");
+// 1. Hent deler av objektene med map() og skriv ut som et array
+console.log("1. Skrevet ut som array med funksjonen map():");
 console.log(dyr.map(x=>x.navn+" er en "+x.art));
 
-// Hent deler av objektene med map() og dkriv ut med forEach
-console.log("Skrevet ut som tekst med forEach():");
+console.log("-------------------------------------");
+
+// 2. Hent deler av objektene med map() og dkriv ut med forEach
+console.log("2. Skrevet ut som tekst med forEach():");
 dyr.forEach(x=>console.log(x.navn+" er en "+x.art));
 
+console.log("-------------------------------------");
 
-// Filtrer etter enkelte felts verdier med filter()
-console.log(dyr.filter(x=>x.art=="Katt"));
-
-// Hent alle katter med filter() og skriv ut som array
-
+// 3. Hent alle katter med filter() og skriv ut som array
 let katter = dyr.filter(x=>x.art=="Katt");
 
-console.log("Liste over katter:");
+console.log("3. Liste over dyr som er katter, filtrert ut med funksjonen filter():");
 
 katter.forEach(katt=>console.log("En katt: "+katt.navn));
 
-// Hent alle katter som heter Ola, med filter()
+console.log("-------------------------------------");
 
-console.log("Katter som heter Ola:");
+// 4. Hent alle katter som heter Ola, med filter()
+
+console.log("4. Liste over katter som heter Ola, filtrert ut med funksjonen filter():");
 
 let Ola = dyr.filter(x=>x.art=="Katt" && x.navn=="Ola");
 console.log(Ola);
 
-console.log("-------------------------");
+console.log("-------------------------------------");
+
+// 5. Skriv ut med map() og en funksjon over flere linjer  
+console.log("5. Skrevet ut med map() og en funksjon over flere linjer ");
 
 
 let setning1 = dyr.map(function(x) {
@@ -44,13 +48,21 @@ return x.navn+" er en "+x.art;
 
 console.log(setning1);
 
-let setning2 = dyr.map((x)=> {
+console.log("-------------------------------------");
+
+// 6. Skriv ut med map() og en fat arrow funksjon over flere linjer  
+console.log("6. Skrevet ut med map() og en \"fat arrow\" funksjon over flere linjer ");
+
+
+let setning2 = dyr.map( x => {
   return x.navn+" er en "+x.art
 });
 console.log(setning2);
 
+console.log("-------------------------------------");
 
-
+// 7.Beregne sum og produkt at et array med tall med reduce()
+console.log("7. Beregne sum og produkt at et array med tall med reduce():\n");
 
 let tall = [1,2,3,4,5,6];
 
