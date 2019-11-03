c771815556764b2dfa3c0551aa132083b90157e1
Aina Berg-Hansen <ainaberg-hansen@Ainas-MacBook-Air.local>
2019-11-03 18:07:21 +0100

    regex

--- reg_uttrykk.js
+++ reg_uttrykk.js
@@ -1,12 +1,11 @@
 // Eksempler på bruk av regulære uttrykk
 
-
 const pattern = /orange/;  
 console.log(pattern.test("orange")); // true
 const patternIgnoreCase = /orange/i;
 console.log(patternIgnoreCase.test("Orange")); // true
 const patternGlobal = /orange/ig;
-console.log(patternGlobal.test("Orange Juice")); // true
+console.log(patternGlobal.test("Orange Juice is orange juice")); // true
 
 
 // Sjekk om et tall eller en streng inneholder ett av tallene i en tallrekke
@@ -22,5 +21,15 @@ console.log(/[0123456789]/.test("This is year 2018")); //true
 const pattern2 = /[^abc]/;
 console.log(pattern2.test('a')); //false
 console.log(pattern2.test('d')); //true
+
+// Sjekk om en streng begynner med ett ord
+const pattern3 = /^Dette/;
+console.log(pattern3.test('Dette stemmer')); //false
+console.log(pattern3.test('Men ikke dette')); //true
+
+
+// Sjekk om enn streng inneholder mer enn en forekomst av et ord
+const pattern4 = /eple/;
+
 
 
