3c30ba68228e674aff408a908aea24c75332b3f0
Terje Berg-Hansen <terje@itfakultetet.no>
2019-11-04 18:49:59 +0100

    eksempler

---
+++ json.js
@@ -0,0 +1,36 @@
+const person =
+{
+    "fornavn": "Ola",
+    "etternavn": "Nordmann",
+    "alder": 25,
+    "adresse":
+    {
+        "gateadresse": "Bakken 4",
+        "postnummer": 1234,
+        "poststed": "Bakkebygrenda"
+    },
+    "telefonnumre":
+        [
+            {
+                "type": "mobil",
+                "nummer": "912 34 567"
+            },
+            {
+                "type": "hjem",
+                "nummer": "12 34 56 78"
+            },
+            {
+                "type": "fax",
+                "nummer": "87 65 43 21"
+            }
+        ],
+    "initialer": function () {
+        return this.fornavn.charAt(0) + "." + this.etternavn.charAt(0) + ".";
+    }
+
+}
+
+
+console.log(person.initialer());
+
+
--- strenger.js
+++ strenger.js
@@ -14,19 +14,34 @@ console.log(str2.link( URL ));
 // eller lag en lenke direkte:
 console.log("Kursportal".link("http://www.itfakultetet.no/kurs"));
 
+// split() og join()
+
+let tegn = "Dette er en setning med bokstaver og mellomrom".split('');
+console.log(tegn);
+
+let setningFraTegn = tegn.join('')
+console.log(setningFraTegn);
+
+let setningMedSkilletegn = "Dette er en setning".split('').join('-');
+console.log(setningMedSkilletegn);
+
+
 // Skriv en setning baklengs:
 let baklengs = "Dette er en setning".split("").reverse().join("");
 console.log(baklengs,'\n');
 
 // Finn en bokstavs indeksverdi i en setning
 let str3 = "Dette er en setning som har bokstaven å i seg";
-
 console.log(str3,'\n');
-
 let indeksverdi = str3.search("å");
-
-
 console.log("\"å\" er bokstav nummer:",indeksverdi);
 
 
+// startsWith(), endsWith() og includes() 
+
+let str4 = "Dette er starten og dette er slutten på teksten";
+
+console.log(str4.startsWith("Dette")); // true
+console.log(str4.endsWith("slutten"));  // false
+console.log(str4.includes("på"));       // true
 
